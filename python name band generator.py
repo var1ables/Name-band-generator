d9c7eeae7a1bf08a0db4587791f495615cf035ec
var1ables <67993562+var1ables@users.noreply.github.com>
2022-01-13 05:33:24 -0800

    Add files via upload

---
+++ python name band generator.py
@@ -0,0 +1,11 @@
+#introductory greeting
+print('Welcome to the name band generator!')
+
+#Part 1 of generator, the city name
+city_name = input('What city did you grow up in?\n')
+
+#Part 2 of geneerator, your favorite pets name
+pets_name = input('Whats your favorite pets name?\n')
+
+#Combining the two variables forms the band name
+print('Your band name could be ' + city_name + " " + pets_name)
